7767fca4131e36e8c6cc4de68cd26aa974f5cde7
alexa <alexandragavril19@gmail.com>
2020-12-30 23:09:28 +0200

    Delete selection complete

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const app = {
   invisibleCanvas: null,
-  canvas: null,
+  canvas: null
 };
+
 
 app.draw = function () {
   console.log(app.canvas.clientHeight);
@@ -14,35 +15,111 @@ app.draw = function () {
 
 app.load = function () {
   app.canvas = document.getElementById("canvas");
+  const context = app.canvas.getContext("2d");
 
   app.invisibleCanvas = document.createElement("canvas");
 
-  const context = app.canvas.getContext("2d");
+  let clickSelectAllBtn = 0; //variabila cu care verificam daca am selectat intreaga imagine
+  let image; //variabila ce va retine un obiect de tip ImageData
+
+  let selectAllBtn = document.getElementById("selectAllBtn");
+
+  //adaugam eveniment de click pe butonul de selectie completa a imaginii
+  selectAllBtn.addEventListener("click", () => {
+    image = context.getImageData(0, 0, canvas.width, canvas.height); //preluam imaginea inainte de a aplica selectia
+    context.beginPath();
+    context.rect(0, 0, app.canvas.width, app.canvas.height);
+    context.stroke();
+    clickSelectAllBtn = 1; //marcam ca imaginea a fost selectata complet
+  });
 
   let x = 0;
-  let y = 0;
+  let y = 0; //x,y - coordonatele punctului de start a selectiei
+
   let x1 = 0;
-  let y1 = 0;
-  let click = 0;
+  let y1 = 0; //x1, y1 - coordonatele finale (sau intermediare) ale selectiei
 
-  app.canvas.addEventListener("mousedown", (ev) => {
-    x = (ev.offsetX * app.canvas.width) / app.canvas.clientWidth;
-    y = (ev.offsetY * app.canvas.height) / app.canvas.clientHeight;
-    console.log(x + " " + y);
+  let click = 0; // variabila cu care verificam daca am finalizat selectia
+  let mouseMove = 0; //variabila care verifica daca selectia este in miscare
+
+  let selectBtn = document.getElementById("selectBtn");
+
+  //adaugam eveniment de click pe butonul de selectie a imaginii
+  selectBtn.addEventListener("click", () => {
+    click = 0; //reinitializam variabila click in caz ca selectia este reapelata
+
+    if (clickSelectAllBtn == 1) {
+      // in caz ca selectia completa a fost aplicata renuntam la ea
+      context.putImageData(image, 0, 0); //aplicam imaginea originala
+      clickSelectAllBtn = 0;
+    }
+
+    //functie pe evenimentul de mousedown asupra canvas-ului care preia coord. pct-ului de start
+    app.canvas.addEventListener("mousedown", (ev) => {
+      if (click == 0) {
+        x = (ev.offsetX * app.canvas.width) / app.canvas.clientWidth;
+        y = (ev.offsetY * app.canvas.height) / app.canvas.clientHeight;
+        image = context.getImageData(0, 0, canvas.width, canvas.height); //retinem imaginea
+        console.log(x + " " + y);
+        mouseMove = 1; //retinem ca mouse-ul este in miscare
+      }
+    });
+
+    //functie atasata evenimentului de mousemove pe canvas care redeseneaza dreptunghiul aferent selectiei
+    app.canvas.addEventListener("mousemove", (ev) => {
+      if (mouseMove == 1) {
+        context.putImageData(image, 0, 0);
+        x1 = (ev.offsetX * app.canvas.width) / app.canvas.clientWidth;
+        y1 = (ev.offsetY * app.canvas.height) / app.canvas.clientHeight;
+        context.beginPath();
+        context.rect(x, y, x1 - x, y1 - y);
+        context.stroke();
+      }
+    });
+
+    //functie atasata ev de mouseup prin care finalizam desenarea dreptunghiului selectiei
+    app.canvas.addEventListener("mouseup", (ev) => {
+      mouseMove = 0; //marcam faptul ca mouse-ul s-a oprit
+      if (click == 0) {
+        context.putImageData(image, 0, 0);
+        x1 = (ev.offsetX * app.canvas.width) / app.canvas.clientWidth;
+        y1 = (ev.offsetY * app.canvas.height) / app.canvas.clientHeight;
+        console.log(x1 + " " + y1);
+        context.beginPath();
+        context.rect(x, y, x1 - x, y1 - y);
+        context.stroke();
+        click = 1; // marcam ca selectia s-a terminat
+      }
+    });
   });
 
-  app.canvas.addEventListener("mouseup", (ev) => {
-    x1 = (ev.offsetX * app.canvas.width) / app.canvas.clientWidth;
-    y1 = (ev.offsetY * app.canvas.height) / app.canvas.clientHeight;
-    console.log(x1 + " " + y1);
-    context.beginPath();
-    context.rect(x, y, x1 - x, y1 - y);
-    context.stroke();
+  let deleteBtn = document.getElementById("deleteBtn");
+
+  //stergere selectie curenta
+  deleteBtn.addEventListener("click", () => {
+    if (x1 > x && y1 > y) {
+      //stergere din coltul stanga sus
+      context.clearRect(x - 1, y - 1, x1 + 2 - x, y1 + 2 - y);
+    } else if (x > x1 && y1 > y) {
+      //dreapta sus
+      context.clearRect(x + 1, y - 1, x1 - x - 2, y1 - y + 2);
+    } else if (x1 < x && y1 < y) {
+      //dreapta jos
+      context.clearRect(x + 1, y + 1, -(x - x1 + 2), -(y - y1 + 2));
+    } else if (x < x1 && y > y1) {
+      //stanga jos
+      context.clearRect(x - 1, y + 1, -(x - x1 - 2), -(y - y1 + 2));
+    }
   });
 
-  let deleteBtn = document.getElementById("delete");
-  deleteBtn.addEventListener("click", (ev) => {
-    context.clearRect(x, y, x1 - x, y1 - y);
+  let downloadBtn = document.getElementById("downloadBtn");
+
+  //salvare poza
+  downloadBtn.addEventListener("click", () => {
+    const a = document.createElement("a");
+    a.href = app.canvas.toDataURL();
+    a.download = "image";
+    a.click();
   });
 
   let inputFile = document.getElementById("inputFile");
@@ -57,19 +134,18 @@ app.load = function () {
       const dataURL = ev.target.result; //preluam url-ul fisierului incarcat
       //console.log(ev.target);
 
-      const img = document.createElement("img");
-      img.addEventListener("load", function (ev) {
+      const image = document.createElement("img");
+      image.addEventListener("load", function (ev) {
         //s-a incarcat imaginea
-        app.canvas.width = app.invisibleCanvas.width = img.naturalWidth;
+        app.canvas.width = app.invisibleCanvas.width = image.naturalWidth;
+        app.canvas.height = app.invisibleCanvas.height = image.naturalHeight;
 
-        app.canvas.height = app.invisibleCanvas.height = img.naturalHeight;
+        const invisibleContext = app.invisibleCanvas.getContext("2d");
+        invisibleContext.drawImage(ev.target, 0, 0);
 
-        const oContext = app.invisibleCanvas.getContext("2d");
-        oContext.drawImage(ev.target, 0, 0);
-
-        app.draw();
+        app.draw(); //functie care incarca imaginea in cadrul canvasului
       });
-      img.src = dataURL; //incepe incarcarea imaginii
+      image.src = dataURL; //incepe incarcarea imaginii
       //este posibil ca imaginea sa nu se fi incarcat
     });
 
